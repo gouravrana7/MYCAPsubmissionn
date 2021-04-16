8e6b7474d10572d203c18554482a498ff5d7e1eb
gouravrana7 <82229078+gouravrana7@users.noreply.github.com>
2021-04-17 00:04:10 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,9 @@
+s=input()
+counter = {}
+for char in s:
+    if char in counter:
+        counter[char] +=1
+    else:
+        counter[char] = 1
+
+print(sorted(counter.items(),key=lambda x : x[1], reverse= True))
